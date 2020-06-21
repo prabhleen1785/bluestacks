388c612386341ddfc1c0ec3a6ab788d6a2d2ac5d
prabhleen1785 <prabhleen1785@gmail.com>
2020-06-22 02:18:05 +0530

    port number eror fix

--- server/index.js
+++ server/index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const pino = require("express-pino-logger")();
-const fs = require('fs');
+const fs = require("fs");
 const app = express();
 const router = express.Router();
-const cors = require('cors');
+const cors = require("cors");
+const port = process.env.PORT || 8080;
 
-var data = fs.readFileSync('../src/data.json');
+var data = fs.readFileSync("../src/data.json");
 var words = JSON.parse(data);
-const {
-  handleRegister
-} = require("./apiHandler")
+const { handleRegister } = require("./apiHandler");
 
 const jsonParser = bodyParser.json();
 
@@ -19,15 +18,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-app.get("/", (req,res) =>{ console.log(words);res.sendFile(path.resolve("../public/index.html"))});
-
-app.get('/data', (req, res) => {
-  res.sendFile(path.resolve('../src/data.json'));
+app.get("/", (req, res) => {
+  console.log(words);
+  res.sendFile(path.resolve("../public/index.html"));
 });
 
-app.post('/register', jsonParser, handleRegister);
+app.get("/data", (req, res) => {
+  res.sendFile(path.resolve("../src/data.json"));
+});
 
+app.post("/register", jsonParser, handleRegister);
 
-app.listen(8000, () =>
-  console.log("Express server is running on localhost:8000")
+app.listen(port, () =>
+  console.log(`Express server is running on localhost:${port}`)
 );
